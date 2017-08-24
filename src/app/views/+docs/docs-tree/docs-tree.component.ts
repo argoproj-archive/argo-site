490e2d09779277ca2618d96723fb7fe9a4dcd2d5
r_pelczar <rafal.pelczar@acaisoft.com>
2017-08-25 00:14:33 +0200

    Collapsing navigation in docs

--- src/app/views/+docs/docs-tree/docs-tree.component.ts
+++ src/app/views/+docs/docs-tree/docs-tree.component.ts
@@ -8,28 +8,13 @@ import { ActivatedRoute, Router } from '@angular/router';
     templateUrl: './docs-tree.html',
     styles: [require('./docs-tree.scss').toString()],
 })
-export class DocsTreeComponent implements OnInit {
+export class DocsTreeComponent {
     @Input()
     public tree: DocsTree = {};
-
-    public currentDoc: string;
-
-    constructor(private route: ActivatedRoute) {
-    }
 
     public get nodes(): DocsTree[] {
         return this.tree.children || [];
     }
-
-    public async ngOnInit() {
-        this.route.params.subscribe(async params => {
-            this.currentDoc = params['doc'] || '';
-        });
-    }
-
-    // public isExpanded(node: DocsTree) {
-    //     for(node)
-    // }
 }
 
 @Component({
@@ -46,8 +31,8 @@ export class DocsTreeNodeComponent implements OnInit {
     @Input()
     public node: DocsTree = {};
 
-    public expanded: boolean;
     public currentDoc: string;
+    public expanded: boolean;
 
     constructor(private route: ActivatedRoute, private router: Router) {
     }
@@ -56,6 +41,7 @@ export class DocsTreeNodeComponent implements OnInit {
         this.route.params.subscribe(async params => {
             this.currentDoc = params['doc'] || '';
         });
+        this.checkActiveChild();
     }
 
     public isActive(doc: string) {
@@ -63,10 +49,34 @@ export class DocsTreeNodeComponent implements OnInit {
     }
 
     public open(node: DocsTree) {
-        this.expanded = true;
+        this.expanded = this.node.path === this.currentDoc ? !this.expanded : true;
 
         if (node.path) {
             this.router.navigate([{ doc: node.path }]);
         }
+    }
+
+    private checkActiveChild() {
+        this.expanded = false;
+
+        if (this.node.path === this.currentDoc) {
+            this.expanded = true;
+        }
+
+        this.hasActiveChild(this.node);
+    }
+
+    private hasActiveChild(node: DocsTree) {
+        if (node.path === this.currentDoc || this.expanded) {
+            this.expanded = true;
+            return;
+        }
+
+        if (node.children) {
+            for (let i = 0; i < node.children.length; i++) {
+                this.hasActiveChild(node.children[i]);
+            }
+        }
+
     }
 }
