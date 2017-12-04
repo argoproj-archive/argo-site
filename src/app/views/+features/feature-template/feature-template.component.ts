c4b40cf56c7e2c2debf4dc942862b1e9105ddc67
r_pelczar <rafal.pelczar@acaisoft.com>
2017-12-04 16:44:36 +0100

    Features

---
+++ src/app/views/+features/feature-template/feature-template.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'argo-feature-template',
+    templateUrl: './feature-template.html',
+    styles: [require('./feature-template.scss').toString()],
+})
+export class FeatureTemplateComponent {
+}
---
+++ src/app/views/+features/features.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+import { PageSettings, HasPageSettings } from '../../common';
+
+@Component({
+    selector: 'argo-features',
+    template: '<router-outlet></router-outlet>',
+})
+export class FeaturesComponent {
+}
